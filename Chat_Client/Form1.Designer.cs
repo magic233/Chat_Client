5c4fbc1a71d3745758f8dfc91646c56150fc7676
magic233 <mywh617171928@live.com>
2017-11-27 23:19:17 +0800

    Initial commit

---
+++ Chat_Client/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Chat_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textChat = new System.Windows.Forms.TextBox();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textChat
+            // 
+            this.textChat.Location = new System.Drawing.Point(234, 12);
+            this.textChat.Multiline = true;
+            this.textChat.Name = "textChat";
+            this.textChat.Size = new System.Drawing.Size(727, 390);
+            this.textChat.TabIndex = 0;
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(234, 408);
+            this.textInput.Multiline = true;
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(727, 128);
+            this.textInput.TabIndex = 1;
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(886, 542);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(75, 23);
+            this.buttonEnter.TabIndex = 2;
+            this.buttonEnter.Text = "发送";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 575);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.textChat);
+            this.Name = "Form1";
+            this.Text = "聊天客户端";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textChat;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Button buttonEnter;
+    }
+}
+
---
+++ Chat_Client/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Chat_Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textChat.Text = textChat.Text + "\nClient Running ...";
+            //TcpClient client; 
+            //try
+            //{
+            //    client = new TcpClient();
+            //    client.Connect("localhost", 8500);
+            //    textChat.Text = textChat.Text + "\n"+"OK ...";
+            //}
+            //catch(Exception ex)
+            //{
+            //    textChat.Text = textChat.Text + "\nServer Connected!" + ex.Message ;
+            //    return;
+            //}
+           
+        }
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            TcpClient client;
+            try
+            {
+                client = new TcpClient();
+                client.Connect("localhost", 8500);
+            }
+            catch (Exception ex)
+            {
+                textChat.Text = textChat.Text + "\n"+"Server Connected!" + ex.Message;
+                return;
+            }
+            NetworkStream streamToServer = client.GetStream();
+
+            byte[] text = Encoding.Unicode.GetBytes(textInput.Text);
+            streamToServer.Write(text, 0, text.Length);
+            int textRead;
+            text = new byte[8192];
+            textRead = streamToServer.Read(text, 0, 8192);
+            string msg = Encoding.Unicode.GetString(text, 0, textRead);
+            textChat.Text = textChat.Text + "\n"  + msg;
+        }
+    }
+}
